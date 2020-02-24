b90348460a1e9b4fbf5d10974f9fd3c208130ea6
TomasRosello <44257397+TomasRosello@users.noreply.github.com>
2020-02-24 17:01:20 +0100

    Clase 24/02
    Giro no probado,
    Velocidad Integral aplicada

--- Robot_Base_ConGiro.ino
+++ Robot_Base_ConGiro.ino
@@ -28,15 +28,16 @@
 #define BAUD_1 38400 // velocidad de comunicación con la controladora
 #define VEL_MAX 40 // velocidad máxima (pulsos/T)
 
-int VELOC_IZQ=10; // velocidades deseadas para las ruedas (en pulsos/T)
-int VELOC_DER=10;
+int VELOC_IZQ=5; // velocidades deseadas para las ruedas (en pulsos/T)
+int VELOC_DER=5;
 
 float d=12.5; // Diámetro de ruedas en cm
-float D=33; // Distancia entre ruedas en cm
+float D=46; // Distancia entre ruedas en cm
 int PPV=980; // pulsos de encoder por vuelta de rueda
 byte accel=2; // aceleración (se alcanza la velocidad deseada en 1.024 seg)
 byte error; // código de error devuelto por la controladora
-int error_total=0;
+int error_total_izq = 0;
+int error_total_der = 0;
 byte ver; // Versión del software de la controladora
 byte tension; // tensión de las baterías
 byte motor; // MOTOR_LEFT o MOTOR_RIGHT
@@ -256,35 +257,72 @@ void Avanzar(int V_izq, int V_der)
   int err_izq,err_der;
   byte pot_izq, pot_der;
   float kp = 1.0;
+  float ki = 0.001;
   long int VELOC_AUX_DER=0;
-  
+  long error_long_izq;
+  long error_long_der;
+
   VELOC_IZQ_AUX=LeerVelocidades(VELOC_AUX_DER);
   BorrarEncoders();
   err_izq=V_izq-VELOC_IZQ_AUX;
   err_der= V_der-VELOC_AUX_DER;
 
-  pot_izq= kp * err_izq + BASE_POT;
-  pot_der= kp * err_der + BASE_POT;
-  
-  
+
+  error_long_izq = (long) error_total_izq;
+  error_long_der = (long) error_total_der;
+
+  if((error_long_izq + err_izq)  > 2147483647){
+    error_total_izq = 2147483647; 
+  }
+  else{
+    error_total_izq += err_izq;
+  }
+
+  if(error_long_der + err_der > 2147483647){
+    error_total_der = 2147483647; 
+  }
+  else{
+    error_total_der += err_der;
+  }
+
+
+  if((error_long_izq + err_izq)  < -2147483647){
+    error_total_izq = -2147483647; 
+  }
+  else{
+    error_total_izq += err_izq;
+  }
+
+  if(error_long_der + err_der < -2147483647){
+    error_total_der = -2147483647; 
+  }
+  else{
+    error_total_der += err_der;
+  }
+ 
+
+  pot_izq= kp * err_izq + ki * error_total_izq + BASE_POT;
+  pot_der= kp * err_der + ki * error_total_der + BASE_POT;
+
+ 
   if(pot_izq > 255)
     pot_izq=MAX_POT;
   if(pot_izq < 0)
-    pot_izq=-MAX_POT;
+    pot_izq=-0;
 
     
   if(pot_der > 255)
-    pot_der=255;
+    pot_der=MAX_POT;
   if(pot_der < 0)
-    pot_der=-MAX_POT;
+    pot_der=0;
    
+  
   
   PonerPotencia(MOTOR_LEFT,pot_izq);
   PonerPotencia(MOTOR_RIGHT,pot_der);
   
-  Serial.print("Izquierda");
-  Serial.println(pot_izq);
-  Serial.print("Derecha");
+
+  Serial.println(VELOC_AUX_DER);
   Serial.println(pot_der);
 
   
@@ -293,6 +331,43 @@ void Avanzar(int V_izq, int V_der)
 //**********************************************************************
 void Rotar(char sentido,int ang) // sentido ---> i: izquierdas; d: derechas
 {
+  int err_izq, err_der, pos_i, pos_d, pos_r;
+  float factor;
+  byte pot_izq, pot_der;
+  long vel_izq_aux = 0;
+  long vel_der_aux = 0;
+  
+  float kp = 0.1;
+
+  factor = (PPV * D) / (360 * d);
+
+  pos_i = 0;
+  pos_d = 0;
+  pos_r = (int) (factor * ang);
+
+
+  if(sentido == i)
+  {
+    while ((pos_i > -pos_r) && (pos_d < pos_r))
+    {
+      err_der = pos_r - pos_d;
+      err_izq = pos_r - pos_i;
+      
+      pot_der = BASE_POT + kp * err_der;
+      pot_izq = BASE_POT - kp * err_izq;
+      
+      PonerPotencia(MOTOR_RIGHT,pot_der);
+      PonerPotencia(MOTOR_LEFT,pot_izq);
+
+      LeerVelocidad(vel_der_aux);
+      LeerVelocidad(vel_izq_aux);
+      
+      pos_d += vel_der_aux;
+      pos_i += vel_izq_aux;
+    }
+
+  }
+  
 }
 
 //**********************************************************************
